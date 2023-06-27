b4516ebe41d4b0c1b28af67738f6ab66cf4b77d2
mcimam <imam123.ci@gmail.com>
2023-06-27 10:43:32 +0700

    update method

--- models/device.py
+++ models/device.py
@@ -1,5 +1,5 @@
 from odoo import api, fields, models, _
-
+from datetime import datetime
 class Device(models.Model):
     _name= 'giot.device'
 
@@ -15,18 +15,36 @@ class DeviceLog(models.Model):
 
     device_id = fields.Many2one('giot.device', string='Device', required=True)
     raw_log = fields.Text(string='Raw JSON Log')
-
-    date = fields.Datetime(string='Date')
+    
+    @api.model
+    def append_log(self,device_mac, logs):
+        # get device id
+        device = self.env['giot.device'].search([('mac','=',str(device_mac))])
+        if not device:
+            return False
+        #check each param
+        self.create({
+            'device_id':device.id,
+            'raw_log' : logs
+        })
+        return True
 
 class DeviceCmd(models.Model):
     _name = 'giot.device.cmd'
     device_id = fields.Many2one('giot.device', string='Device', required=True)
-
-    method = fields.Selection([('reset','reset'),('set','set')], string='Method', required=True)
+    method = fields.Selection([('ping', 'ping'),('reset','reset'),('set','set')], string='Method', required=True)
     params = fields.Char(string='Params')
+    is_return  = fields.Boolean(string='Return Status', default=False, readonly=True)
+    is_success = fields.Boolean(string='Success Status', default=False, readonly=True)
+    
+    return_data = fields.Text(string='Return Data', readonly=True)
+    return_time = fields.Datetime(string='Return Time', readonly=True)
 
-    is_executed = fields.Boolean(string='Execution Status', default=False)
-    is_suceess = fields.Boolean(string='Success Status')
-    is_return  = fields.Boolean(string='Return Status', default=False)
-
-    return_data = fields.Text(string='Return Data')
+    @api.model
+    def rtr_cmd(self, cmd_id, rtr_data):
+        cmd = self.browse(cmd_id)
+        cmd.return_data = rtr_data
+        cmd.is_success = True
+        cmd.return_time = datetime.now()
+        
+        return True
